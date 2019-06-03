be809ad85b3117ca44c374ddb324ecc8247fb1d7
Francois Huyghe <francois@13milliseconds.com>
2019-06-03 11:35:26 -0700

    add iframe to Head

--- components/QuizContainer.js
+++ components/QuizContainer.js
@@ -1,4 +1,5 @@
 import '../style/style.scss'
+import Head from 'next/head'
 
 class QuizContainer extends React.Component {
 
@@ -6,6 +7,12 @@ class QuizContainer extends React.Component {
 
     return (
       <div className={"quizContainer " + this.props.name}>
+        <Head>
+          <title>Quizr</title>
+          <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+          <script src="../lib/iframeResizer.contentWindow.min.js" type="text/javascript"></script>
+        </Head>
+        
         <div className="gradient">  
           <div className="wrap">
             { this.props.children }
