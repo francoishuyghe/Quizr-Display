de3b1ef3a1bc564d5747a6153f0fe62c0c8d42a2
Francois Huyghe <francois@13milliseconds.com>
2019-07-19 17:54:52 -0500

    Added Babel Polyfills in a better way

--- next.config.js
+++ next.config.js
@@ -1,13 +1,29 @@
 const withSass = require('@zeit/next-sass')
-const webpack = require('webpack');
+const webpack = require('webpack')
 
 module.exports = withSass({
   webpack: config => {
+    const originalEntry = config.entry
+    
+    config.entry = async () => {
+      const entries = await originalEntry()
+
+      if (
+        entries['main.js'] &&
+        !entries['main.js'].includes('./client/polyfills.js')
+      ) {
+        entries['main.js'].unshift('./client/polyfills.js')
+      }
+
+      return entries
+    }
+
     const env = { 
       APP_URL: JSON.stringify(process.env.APP_URL),
       SENDGRID_API_KEY: JSON.stringify(process.env.SENDGRID_API_KEY)
     };
     config.plugins.push(new webpack.DefinePlugin(env));
+
     return config;
   }
 });
