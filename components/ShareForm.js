93d77ef64b16d5c800fd56077aad63fb7f6a3907
Francois Huyghe <francois@13milliseconds.com>
2020-03-03 13:03:57 -0600

    Updating Coupon activation

--- components/ShareForm.js
+++ components/ShareForm.js
@@ -31,7 +31,7 @@ class ShareForm extends React.Component {
                 <div className="alert">{error.general}</div>}
             {isSaving
                 ? <a className="btn">Saving...</a>
-                : <a onClick={() => this.props.sendEmail(this.state.data)} className="btn">Send my Results{coupons._id && !coupons.discountPaused && coupons.discountCodes.length > 0 && " + Discount code"}</a>
+                : <a onClick={() => this.props.sendEmail(this.state.data)} className="btn">Send my Results{coupons._id && settings.couponGeneral && coupons.discountCodes.length > 0 && " + Discount code"}</a>
             }
       </form>
     }
--- components/ShareFormTradeshow.js
+++ components/ShareFormTradeshow.js
@@ -115,7 +115,7 @@ class ShareForm extends React.Component {
                 <div className="alert">{ error.general }</div>}
             {isSaving
                 ? <a className="btn">Saving...</a>
-                : <a onClick={() => this.props.sendEmail(this.state.data)} className="btn">Send my Results{coupons._id && !coupons.discountPausedTradeshow && coupons.discountCodes.length > 0 && " + Discount code"}</a>
+                : <a onClick={() => this.props.sendEmail(this.state.data)} className="btn">Send my Results{coupons._id && settings.couponTradeshow && coupons.discountCodes.length > 0 && " + Discount code"}</a>
             }
       </form>
     }
